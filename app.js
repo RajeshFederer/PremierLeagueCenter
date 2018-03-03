53a32841576090a2c8738a3b0ad2029768a5f8c7
RajeshFederer <rajeshfederer2@gmail.com>
2018-03-03 08:07:24 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.listen(port, function(){
+    console.log('AGENT is running my app on  PORT: ' + port);
+});
