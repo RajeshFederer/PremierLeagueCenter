d85cee4e7720d50c192583324029e6b75a4d71b9
RajeshFederer <rajeshfederer2@gmail.com>
2018-03-03 08:18:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,29 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const Alexa = require("alexa-sdk");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+let handlers = {
+'standings': function() {
+        this.emit(':ask', 'Hello World');
+    }
+};
+
+app.post('/', (req, res) =>{
+    console.log('I am In');
+    let alexa = Alexa.handler(event, context);
+    alexa.appId = 'amzn1.ask.skill.1278c968-0b93-421e-80f8-039f6047063a';
+    //alexa.resources = languageStrings;
+    //alexa.dynamoDBTableName = 'UltracalQuestions';
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+});
 
 app.listen(port, function(){
     console.log('AGENT is running my app on  PORT: ' + port);
