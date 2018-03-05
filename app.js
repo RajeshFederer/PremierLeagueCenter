d2b8a4d73fe84cc0091b691ab3141c8ea56a388e
Rajeshwaran <rajeshfederer2@gmail.com>
2018-03-05 15:44:06 +0530

    test

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+import { handler } from './C:/Users/39850/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/alexa-sdk';
+
 'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const Alexa = require("alexa-sdk");
+const handlers = require('./lib/alexa');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,22 +12,9 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-let handlers = {
-'standings': function(req,resp) {
-        resp.speak('Hello World');
-    }
-};
-
 app.post('/', (req, res) =>{
     console.log('I am In',JSON.stringify(req.body));
-    res.json({"response": {
-        "outputSpeech": {
-          "type": "PlainText",
-          "text": "Hello Jesh"
-        },
-        "shouldEndSession": false
-      }});
-    
+    return handler[req.body.request.intent.name](req, res);
     //let alexa = Alexa.handler(event, context);
     //alexa.appId = 'amzn1.ask.skill.1278c968-0b93-421e-80f8-039f6047063a';
     //alexa.resources = languageStrings;
