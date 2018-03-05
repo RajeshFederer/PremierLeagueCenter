c633be5324d58ce150a12274b677bcfb4adb0dbe
Rajeshwaran <rajeshfederer2@gmail.com>
2018-03-05 16:00:07 +0530

    getting standings

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import { handler } from './C:/Users/39850/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/alexa-sdk';
-
 'use strict';
 
 const express = require('express');
@@ -14,7 +12,7 @@ app.use(bodyParser.json());
 
 app.post('/', (req, res) =>{
     console.log('I am In',JSON.stringify(req.body));
-    return handler[req.body.request.intent.name](req, res);
+    return handlers[req.body.request.intent.name](req, res);
     //let alexa = Alexa.handler(event, context);
     //alexa.appId = 'amzn1.ask.skill.1278c968-0b93-421e-80f8-039f6047063a';
     //alexa.resources = languageStrings;
